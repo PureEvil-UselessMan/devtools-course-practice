bb8fd8dda60286d8283fcd4681bf6c7612b26288
PureEvil-UselessMan <OlyninAlex@gmail.com>
2022-04-24 13:59:53 +0300

    add constructor copy

--- modules/olynin_a_cross_line_and_plane/include/cross_line_and_plane.h
+++ modules/olynin_a_cross_line_and_plane/include/cross_line_and_plane.h
@@ -12,6 +12,8 @@ class Line {
     Line(float tx_1, float ty_1, float tz_1,
          float ta_x, float ta_y, float ta_z):
         x_1(tx_1), y_1(ty_1), z_1(tz_1), a_x(ta_x), a_y(ta_y), a_z(ta_z) {}
+    Line(const Line& Tmp) : x_1(Tmp.x_1), y_1(Tmp.y_1), z_1(Tmp.z_1),
+        a_x(Tmp.a_x), a_y(Tmp.a_y), a_z(Tmp.a_z) {}
 };
 
 
--- modules/olynin_a_cross_line_and_plane/test/test_cross_line_and_plane.cpp
+++ modules/olynin_a_cross_line_and_plane/test/test_cross_line_and_plane.cpp
@@ -6,3 +6,9 @@
 TEST(Class_Line, Constructor_no_throw) {
     EXPECT_NO_THROW(Line(1.1, 3.2, 2.0, 1.0, 0.0, 3.3));
 }
+
+TEST(Class_Line, Copy_constructor_no_throw) {
+    Line AB(1.1, 3.2, 2.0, 1.0, 0.0, 3.3);
+    EXPECT_NO_THROW(Line CD(AB));
+}
+
